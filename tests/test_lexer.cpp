aa367a2b9c630ad782ab7373e7745f33d153ccce
Jakub Konka <kubkon@jakubkonka.com>
2017-04-23 15:35:18 +0100

    Test multiple arguments in function header

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -1,39 +1,54 @@
 #include <gtest/gtest.h>
 #include <sstream>
+#include <string>
+#include <vector>
 #include "lexer.h"
 
 namespace kaleidoscope {
-  TEST(Lexer, FunctionPrototype) {
-    {
+  TEST(FunctionPrototype, SingleArgument) {
+    std::vector<std::string> inputs = {
+      "def fib(x)\n",
+      "def fib (x)\n",
+      "def fib ( x)\n",
+      "def fib (x )\n",
+      "def fib ( x ) \n"
+    };
+    for (const auto& input : inputs) {
       std::stringstream ss;
-      ss << "def(x)\n";
+      ss << input;
       Lexer lexer(ss);
       ASSERT_EQ(Lexer::tok_def, lexer.gettok());
+      ASSERT_EQ(Lexer::tok_identifier, lexer.gettok());
+      ASSERT_EQ(lexer.token_value().identifier_str, "fib");
       ASSERT_EQ('(', lexer.gettok());
       ASSERT_EQ(Lexer::tok_identifier, lexer.gettok());
       ASSERT_EQ(lexer.token_value().identifier_str, "x");
       ASSERT_EQ(')', lexer.gettok());
       ASSERT_EQ(Lexer::tok_eof, lexer.gettok());
     }
-    {
+  }
+
+  TEST(FunctionPrototype, MultipleArguments) {
+    std::vector<std::string> inputs = {
+      "def fib(x,y)\n",
+      "def fib (x,y)\n",
+      "def fib ( x, y)\n",
+      "def fib (x, y )\n",
+      "def fib ( x , y ) \n"
+    };
+    for (const auto& input : inputs) {
       std::stringstream ss;
-      ss << "def (x)\n";
+      ss << input;
       Lexer lexer(ss);
       ASSERT_EQ(Lexer::tok_def, lexer.gettok());
+      ASSERT_EQ(Lexer::tok_identifier, lexer.gettok());
+      ASSERT_EQ(lexer.token_value().identifier_str, "fib");
       ASSERT_EQ('(', lexer.gettok());
       ASSERT_EQ(Lexer::tok_identifier, lexer.gettok());
       ASSERT_EQ(lexer.token_value().identifier_str, "x");
-      ASSERT_EQ(')', lexer.gettok());
-      ASSERT_EQ(Lexer::tok_eof, lexer.gettok());
-    }
-    {
-      std::stringstream ss;
-      ss << "def ( x ) \n";
-      Lexer lexer(ss);
-      ASSERT_EQ(Lexer::tok_def, lexer.gettok());
-      ASSERT_EQ('(', lexer.gettok());
+      ASSERT_EQ(',', lexer.gettok());
       ASSERT_EQ(Lexer::tok_identifier, lexer.gettok());
-      ASSERT_EQ(lexer.token_value().identifier_str, "x");
+      ASSERT_EQ(lexer.token_value().identifier_str, "y");
       ASSERT_EQ(')', lexer.gettok());
       ASSERT_EQ(Lexer::tok_eof, lexer.gettok());
     }
